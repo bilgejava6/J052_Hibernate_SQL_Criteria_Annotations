b421d46ba512f2e4e16b29af31c752f0c32f9fcd
bilgejava6 <bilgejava6@gmail.com>
2025-01-25 10:02:51 +0300

    Criteria Sorguları devam...

--- src/main/java/com/muhammet/criteria/CriteriaExample.java
+++ src/main/java/com/muhammet/criteria/CriteriaExample.java
@@ -4,9 +4,7 @@ import com.muhammet.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 
 import java.util.List;
 
@@ -47,6 +45,34 @@ public class CriteriaExample {
         query.where(cb.equal(root.get("name"),name)); // where root.name = name
         return em.createQuery(query).getResultList();
     }
+
+    // select * from tbluser where name=? or surname=?
+    public List<User> findAllByNameOrSurname(String name, String surname){
+        CriteriaQuery<User> query = cb.createQuery(User.class);
+        Root<User> root = query.from(User.class); // from tbluser
+        query.select(root); // select *
+        Predicate pName = cb.equal(root.get("name"),name); // root.name = name
+        Predicate pSurname = cb.equal(root.get("surname"),surname); // root.surname = surname
+        query.where(cb.or(pName,pSurname)); // where root.name = name or root.surname = surname
+        return em.createQuery(query).getResultList();
+    }
+
+    /**
+     * select id, username,password from tbluser where name like '%a%'
+     * Object[]
+     */
+    public List<Object[]> findManyColumnLikeName(String name){
+        CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
+        Root<User> root = query.from(User.class); // from tbluser
+        Path<Long> id = root.get("id");
+        Path<String> userName = root.get("userName");
+        Path<String> password = root.get("password");
+        query.multiselect(id,userName,password); // select id, username,password
+        query.where(cb.like(root.get("name"), "%"+name+"%"));
+        return em.createQuery(query).getResultList();
+    }
+
+
 
 
 
--- src/main/java/com/muhammet/criteria/RunnerExample.java
+++ src/main/java/com/muhammet/criteria/RunnerExample.java
@@ -5,7 +5,13 @@ public class RunnerExample {
         CriteriaExample cr = new CriteriaExample();
         //cr.findAll().forEach(System.out::println);
 //        cr.selectOneColumn().forEach(System.out::println);
-        cr.findAllByName("Muhammet").forEach(System.out::println);
-
+      //  cr.findAllByName("Muhammet").forEach(System.out::println);
+       // cr.findAllByNameOrSurname("Muhammet", "TAŞ").forEach(System.out::println);
+        /**
+         * List<Object[]>
+         */
+        cr.findManyColumnLikeName("e").forEach(u->{
+            System.out.println(u[0]+ " - " + u[1] + " - " + u[2]);
+        });
     }
 }
